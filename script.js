92a3e2b755b27d85c55a26c6bd162592fc2e0910
Faith Fechser <faithfechser@gmail.com>
2023-04-02 08:26:36 -0600

    add password generating forloop and general character variable

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ var charLetterUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var charNumeric = "0123456789"
 var charSpecial = "!?.-_@#$*~"
 
-//random password function
-function passwordGen(){
+//password generator function
+function generatePassword(){
   // character user prompt
   var passLength = prompt("How long would you like your password to be? (Password may contain 8 to 128 characters)")
   // password length alert system
@@ -22,8 +22,34 @@ function passwordGen(){
   var useNumeric = confirm("Should your password include numeric characters?")
   var useSpecial = confirm("Should your password include special characters?")
 
-
+  if (!useLetterLow && !useLetterUp && !useNumeric && !useSpecial){
+    alert("At least one character type must be selected.")
+    return "";
+  }
 }
+
+// turning character types into strings
+var character = "";
+if (useLetterLow) {
+  character += charLetterLow;
+}
+if (useLetterUp) {
+  character += charLetterUp;
+}
+if (useNumeric) {
+  character += charNumeric;
+}
+if (useSpecial) {
+  character += charSpecial;
+}
+
+//password generating forloop
+var password = "";
+for (var i = 0; i < passwordLength; i++) {
+  password += character.charAt(Math.floor(Math.random() * character.length));
+}
+return password;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -33,13 +59,6 @@ function writePassword() {
 
 }
 
-// Write password to the #password input
-function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
-
-passwordText.value = password;
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
