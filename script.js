e3b5fcc44a16e523482b276d82255803cb6ed78e
Faith Fechser <faithfechser@gmail.com>
2023-04-02 00:03:49 -0600

    add character usage variables and add if statements to a password generating function

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ var generateBtn = document.querySelector("#generate");
 // global character variables
 var charLetterLow = "abcdefghijklmnopqrstuvwxyz"
 var charLetterUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var charNumberic = "0123456789"
+var charNumeric = "0123456789"
 var charSpecial = "!?.-_@#$*~"
 
-// character user prompt
-var passLength = prompt("How long would you like your password to be? (Password may contain 8 to 128 characters)")
+//random password function
+function passwordGen(){
+  // character user prompt
+  var passLength = prompt("How long would you like your password to be? (Password may contain 8 to 128 characters)")
+  // password length alert system
+  if (passLength <8 || passLength > 128 || isNaN(passLength)) {
+    alert("Input error, password may only contain 8 to 128 characters.")
+    return "";
+  }
+  //local character type usage variables
+  var useLetterLow = confirm("Should your password include lowercase letters?")
+  var useLetterUp = confirm("Should your password include uppercase letters?")
+  var useNumeric = confirm("Should your password include numeric characters?")
+  var useSpecial = confirm("Should your password include special characters?")
 
-if (passLength <8 || passLength > 128 || isNaN(passLength)) {
-  alert("Input error, password may only contain 8 to 128 characters.")
-  return "";
+
 }
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
