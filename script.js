73c43ebc0d191282cfe726c32a80499a0e50072b
Faith Fechser <faithfechser@gmail.com>
2023-04-01 23:54:48 -0600

    add password length 'if/else' prompt code

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// global variables
-var characterLetterLow = "abcdefghijklmnopqrstuvwxyz"
-var characterLetterUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var characterNumberic = "0123456789"
-var characterSpecial = "!?.-_@#$*~"
+// global character variables
+var charLetterLow = "abcdefghijklmnopqrstuvwxyz"
+var charLetterUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charNumberic = "0123456789"
+var charSpecial = "!?.-_@#$*~"
+
+// character user prompt
+var passLength = prompt("How long would you like your password to be? (Password may contain 8 to 128 characters)")
+
+if (passLength <8 || passLength > 128 || isNaN(passLength)) {
+  alert("Input error, password may only contain 8 to 128 characters.")
+  return "";
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -14,6 +22,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
 }
 
 // Add event listener to generate button
